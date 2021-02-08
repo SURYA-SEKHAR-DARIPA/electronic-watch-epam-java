0f34651675d9190db3be5b725950dae72e014988
thejerome <thejerome@mail.ru>
2021-02-08 14:38:43 +0300

    Initial Commit

---
+++ src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -0,0 +1,12 @@
+package com.epam.rd.autotasks.meetautocode;
+
+import java.util.Scanner;
+
+public class ElectronicWatch {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int seconds = scanner.nextInt();
+
+
+    }
+}
